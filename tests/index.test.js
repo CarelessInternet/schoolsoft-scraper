0fbf166c27e43e5e845ef66d162663b45f63469b
CarelessInternet <myinternetisg00d@gmail.com>
2021-10-04 21:53:09 +0200

    added environment variables to testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,19 @@
-const SchoolSoft = require('../src/index.js');
+require('dotenv').config();
 
+const SchoolSoft = require('../src/index.js');
 const school = new SchoolSoft('engelska', '/usr/bin/chromium-browser');
+
+test('get username typeof error', async () => {
+	await expect(school.login(12345, 'yee')).rejects.toBe(
+		'Username must be of type string'
+	);
+});
+
+test('get password typeof error', async () => {
+	await expect(school.login('yee', 12345)).rejects.toBe(
+		'Password must be of type string'
+	);
+});
 
 test('get invalid credentials', async () => {
 	await expect(school.login('bruh', 'moment')).rejects.toBe(
@@ -8,22 +21,25 @@ test('get invalid credentials', async () => {
 	);
 });
 
-// test(
-// 	'login to schoolsoft',
-// 	async () => {
-// 		await expect(
-// 			school.login('credentials', 'credentials')
-// 		).resolves.toBe(true);
-// 	},
-// 	10 * 1000
-// );
+test(
+	'login to schoolsoft',
+	async () => {
+		await expect(
+			school.login(
+				process.env.SCHOOLSOFT_USERNAME,
+				process.env.SCHOOLSOFT_PASSWORD
+			)
+		).resolves.toBe(true);
+	},
+	10 * 1000
+);
 
 test(
 	'grab lunch menu',
 	async () => {
-		await expect(school.getLunchMenu()).resolves.toBe(
-			'Lunch menu 39, 27 sep - 1 okt'
-		);
+		await expect(school.getLunchMenu()).resolves.toMatchObject({
+			heading: 'Lunch menu 40, 4 okt - 8 okt'
+		});
 	},
 	15 * 1000
 );
