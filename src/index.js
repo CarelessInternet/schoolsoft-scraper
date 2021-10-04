65b0fd6c3c724e188c85c0cc857e0cc190eb53d3
CarelessInternet <myinternetisg00d@gmail.com>
2021-10-04 20:58:57 +0200

    added more jsdoc stuff and added incognito tabs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@ const puppeteer = require('puppeteer-core');
 
 /**
  * The SchoolSoft class, everything is performed in here
+ * @example <caption>CommonJS</caption>
+ * const SchoolSoft = require('schoolsoft-scraper');
+ * @example <caption>ES6 Import</caption>
+ * import SchoolSoft from 'schoolsoft-scraper';
  */
 class SchoolSoft {
 	/**
+	 * @private
 	 * @type {Boolean}
 	 */
 	#loggedIn = false;
 	/**
+	 * @private
 	 * @type {puppeteer.LaunchOptions}
 	 */
 	#puppeteerOptions = {};
 	/**
+	 * @private
 	 * @type {puppeteer.Browser|undefined}
 	 */
 	#browser = null;
 	/**
+	 * @private
 	 * @type {puppeteer.Page|undefined}
 	 */
 	#page = null;
@@ -24,10 +32,25 @@ class SchoolSoft {
 	/**
 	 * Creates the SchoolSoft class with everything important
 	 * @param {String} school - The school being accessed
-	 * @param {String} [path] - The path for the chromium executable
+	 * @param {String} [path] - The puppeteer path for the chromium executable
+	 * @example <caption>Without chromium path</caption>
+	 * const SchoolSoft = require('schoolsoft-scraper');
+	 * const school = new SchoolSoft('gotland');
+	 * @example <caption>With chromium path</caption>
+	 * const SchoolSoft = require('schoolsoft-scraper');
+	 * const school = new SchoolSoft('engelska', '/usr/bin/chromium-browser');
+	 * @example <caption>ES6 Import</caption>
+	 * import SchoolSoft from 'schoolsoft-scraper';
+	 * const school = new SchoolSoft('medborgarskolan');
 	 */
 	constructor(school, path = '') {
+		/**
+		 * @type {String}
+		 */
 		this.school = school;
+		/**
+		 * @type {String}
+		 */
 		this.baseURL = `https://sms14.schoolsoft.se/${school}/jsp`;
 		this.#puppeteerOptions = {
 			headless: true,
@@ -36,18 +59,21 @@ class SchoolSoft {
 	}
 
 	/**
-	 * Launches puppeteer and opens a page
+	 * Launches puppeteer with an incognito tab and opens a page
 	 * @async
+	 * @private
 	 * @returns {Promise<Boolean>|Promise<String>} Returns the browser and page instance
+	 * @example
+	 * await this.#open()
+	 * .catch(console.error);
 	 */
 	#open() {
 		return new Promise(async (resolve, reject) => {
 			try {
-				const browser = await puppeteer.launch(this.#puppeteerOptions);
-				const page = await browser.newPage();
+				this.#browser = await puppeteer.launch(this.#puppeteerOptions);
+				const context = await this.#browser.createIncognitoBrowserContext();
+				this.#page = await context.newPage();
 
-				this.#browser = browser;
-				this.#page = page;
 				resolve(true);
 			} catch (err) {
 				reject(err);
@@ -62,20 +88,26 @@ class SchoolSoft {
 	 * @param {String} password - The password for the login
 	 * @param {{0, 1, 2}} [userType] - The type of user requesting the login, default is 1 for student, 0 for staff, and 2 for guardians
 	 * @returns {(Promise<Boolean>|Promise<String>)} Returns the initial fetch request to the login page on success
+	 * @example
+	 * try {
+	 * 	await school.login('sample', 'text');
+	 * } catch(err) {
+	 * 	console.error(err);
+	 * }
 	 */
 	login(username, password, userType = 1) {
 		return new Promise(async (resolve, reject) => {
-			if (typeof username !== 'string') {
-				reject('Username must be of type string');
-			}
-			if (typeof password !== 'string') {
-				reject('Password must be of type string');
-			}
-			if (typeof userType !== 'number') {
-				reject('The user type must be a number');
-			}
-
 			try {
+				if (typeof username !== 'string') {
+					throw 'Username must be of type string';
+				}
+				if (typeof password !== 'string') {
+					throw 'Password must be of type string';
+				}
+				if (typeof userType !== 'number') {
+					throw 'The user type must be a number';
+				}
+
 				await this.#open();
 				await this.#page.goto(`${this.baseURL}/Login.jsp?usertype=${userType}`);
 
@@ -88,7 +120,7 @@ class SchoolSoft {
 				const url = this.#page.url();
 
 				if (url !== `${this.baseURL}/student/right_student_startpage.jsp`) {
-					reject('Invalid username or password');
+					throw 'Invalid username or password';
 				}
 
 				this.#loggedIn = true;
@@ -102,13 +134,17 @@ class SchoolSoft {
 	/**
 	 * Gets the lunch menu
 	 * @async
-	 * @returns {Promise<String>} Returns the lunch menu
+	 * @returns {Promise<Object>} Returns the lunch menu in object form
+	 * @example
+	 * school.getLunchMenu()
+	 * .then(console.log)
+	 * .catch(console.error);
 	 */
 	getLunchMenu() {
 		return new Promise(async (resolve, reject) => {
 			try {
 				if (!this.#loggedIn) {
-					reject('User is not logged in');
+					throw 'User is not logged in';
 				}
 
 				await this.#page.goto(
@@ -120,7 +156,7 @@ class SchoolSoft {
 				);
 				const heading = await headingHandle.evaluate((el) => el.innerText);
 
-				resolve(heading);
+				resolve({ heading });
 			} catch (err) {
 				reject(err);
 			}
@@ -130,15 +166,16 @@ class SchoolSoft {
 	/**
 	 * Closes the browser, this should only be called after you are done with your session
 	 * @async
-	 * @returns {Promise<Boolean>|Promise<String>} Returns success on browser close, or rejects with error
+	 * @returns {Promise<Boolean>|Promise<String>} Returns true on browser close, or rejects with error
+	 * @example
+	 * await school.close()
+	 * .catch(console.error);
 	 */
 	close() {
 		return new Promise(async (resolve, reject) => {
 			try {
 				if (!this.#browser) {
-					reject(
-						'An instance of puppeteer has not been initiated, you may not close the browser'
-					);
+					throw 'An instance of puppeteer has not been initiated, you may not close the browser';
 				}
 
 				await this.#browser.close();
